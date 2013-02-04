b3dfcdf356067094bf374afe90b24eac0237248b
Alex Elia <alex@coderpages.com>
2013-02-04 13:58:09 -0800

    Started on Calendar Code
    I'm struggling to figure out how to fill the grid with days. I still
    need calendar logic to define how many days are in the month.

--- TweakDroid/src/com/example/tweakdroid/MainActivity.java
+++ TweakDroid/src/com/example/tweakdroid/MainActivity.java
@@ -1,7 +1,7 @@
 //Package name
 package com.example.tweakdroid;
 
-//Possible importable libraries
+//Possible import-able libraries
 import android.app.Activity;
 import android.content.ContentValues;
 import android.database.Cursor;
@@ -33,10 +33,7 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
-
-
-	
+public class MainActivity extends Activity implements OnClickListener {
 	
 /** Called when the activity is first created. */
 @Override
@@ -46,11 +43,49 @@ public void onCreate(Bundle savedInstanceState) {
 
     //Define variables
     Calendar calendar;
+    int month;
+    int year;
+    int day;
+    ImageView previousMonth;
+    ImageView nextMonth;
+    GridView calendarGrid;
+    int currentDay;
     
     //Make a calendar exist
     calendar = Calendar.getInstance(Locale.getDefault());
-
+    month = calendar.get(Calendar.MONTH);
+    year = calendar.get(Calendar.YEAR);
+    currentDay = calendar.get(Calendar.DAY_OF_WEEK);
     
+    //set a previous month view
+	previousMonth = (ImageView) this.findViewById(R.id.preMonth);
+	previousMonth.setOnClickListener(this);
+	
+	//set a next month view
+	nextMonth = (ImageView) this.findViewById(R.id.nexMonth);
+	nextMonth.setOnClickListener(this);
+
+	//Make a calendar view on the grid view
+	//TODO find a way to throw stuff ON the Grid View
+	calendarGrid = (GridView) this.findViewById(R.id.calendar);	
+	calendar.add(Calendar.DAY_OF_YEAR, Calendar.SUNDAY - currentDay);
+	int gridColumn = 7; 
+	int gridRow = 6;
+	for (int i = 0; i<gridRow; i++){
+		for (int j = 0; j< gridColumn;j++){
+			System.out.print(calendar.get(Calendar.DAY_OF_MONTH));
+			System.out.print(" ");
+			calendar.add(Calendar.DAY_OF_YEAR, 1);
+		}
+		System.out.print("");
+	}
 }
+
+@Override
+public void onClick(View v) {
+	// TODO set onClick events for buttons
+	
+}	
 }
+
 
